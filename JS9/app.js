778c7aad9683a0662b61da376b87ea2271443de9
ninivakh <120251081+ninivakh@users.noreply.github.com>
2023-05-26 15:21:41 +0300

    Add files via upload

---
+++ JS9/app.js
@@ -0,0 +1,9 @@
+function something(){
+    let email = document.getElementById('email');
+    email.value = "";
+
+    let password = document.getElementById('password');
+    password.value = "";  
+
+    return false
+}
